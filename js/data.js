9334968fcf778725d8454c938a942588e752fbca
Vo Hoang Tuan <110124255@st.tvu.edu.vn>
2026-07-22 08:21:38 +0700

    Update data.js

---
+++ js/data.js
@@ -0,0 +1,98 @@
+const dsSanPham = [
+
+{
+    maSP:"SP001",
+    tenSP:"iPhone 15 Pro",
+    giaSP:30000000,
+    moTa:"Điện thoại cao cấp Apple",
+    mauSac:[
+        {
+            maMau:"BLACK",
+            tenMau:"Titan Đen",
+            hinhAnh:"images/iphone-black.jpg"
+        },
+        {
+            maMau:"BLUE",
+            tenMau:"Titan Xanh",
+            hinhAnh:"images/iphone-blue.jpg"
+        }
+    ]
+},
+
+{
+    maSP:"SP002",
+    tenSP:"Samsung Galaxy S24",
+    giaSP:25000000,
+    moTa:"Điện thoại Samsung cao cấp",
+    mauSac:[
+        {
+            maMau:"BLACK",
+            tenMau:"Đen",
+            hinhAnh:"images/samsung-black.jpg"
+        },
+        {
+            maMau:"GRAY",
+            tenMau:"Xám",
+            hinhAnh:"images/samsung-gray.jpg"
+        }
+    ]
+},
+
+{
+    maSP:"SP003",
+    tenSP:"Oppo Reno 11",
+    giaSP:12000000,
+    moTa:"Camera đẹp, pin lớn",
+    mauSac:[
+        {
+            maMau:"BLACK",
+            tenMau:"Đen",
+            hinhAnh:"images/oppo-black.jpg"
+        },
+        {
+            maMau:"GREEN",
+            tenMau:"Xanh Lá",
+            hinhAnh:"images/oppo-green.jpg"
+        }
+    ]
+},
+
+{
+    maSP:"SP004",
+    tenSP:"Xiaomi 14",
+    giaSP:18000000,
+    moTa:"Hiệu năng mạnh",
+    mauSac:[
+        {
+            maMau:"BLACK",
+            tenMau:"Đen",
+            hinhAnh:"images/xiaomi-black.jpg"
+        },
+        {
+            maMau:"BLUE",
+            tenMau:"Xanh",
+            hinhAnh:"images/xiaomi-blue.jpg"
+        }
+    ]
+},
+
+{
+    maSP:"SP005",
+    tenSP:"Vivo V30",
+    giaSP:10000000,
+    moTa:"Thiết kế đẹp",
+    mauSac:[
+        {
+            maMau:"BLACK",
+            tenMau:"Đen",
+            hinhAnh:"images/vivo-black.jpg"
+        },
+        {
+            maMau:"BLUE",
+            tenMau:"Xanh",
+            hinhAnh:"images/vivo-blue.jpg"
+        }
+    ]
+}
+
+];
